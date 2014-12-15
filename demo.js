1c1500bc5fbe6b79617401c234abc2fc1f96c515
Hector Zarco <hezarco@gmail.com>
2014-12-16 00:09:18 +0100

    Add options showcase

--- demo.js
+++ demo.js
@@ -1,13 +1,27 @@
-$(document).ready(function() {
-  $('.show').on('click', show);
-  $('.hide').on('click', hide);
+(function() {
+  $(document).ready(init);
 
-  function show() {
-    var selector = $(this).attr('data-selector');
-    Focusable.setFocus($(selector));
+  function init() {
+    $('.show').on('click', show);
+    $('.hide').on('click', hide);
+
+    function show() {
+      var selector = $(this).attr('data-selector');
+      var options = getOptions();
+      Focusable.setFocus($(selector), options);
+    }
+
+    function hide() {
+      Focusable.hide();
+    }
   }
 
-  function hide() {
-    Focusable.hide();
+  function getOptions() {
+    return {
+      fadeDuration: parseInt($('#fade-duration').val()),
+      hideOnClick: $('#hide-on-click').is(':checked'),
+      hideOnESC: $('#hide-on-esc').is(':checked'),
+      findOnResize: $('#find-on-resize').is(':checked')
+    };
   }
-});
+})();
