1fdba4784f60f2aa25e96313a1bc857d7754485f
Hector Zarco <hezarco@gmail.com>
2014-12-23 13:24:30 +0100

    Don't wrapp columns into wrapper without pointer events

---
+++ sample_app/demo.js
@@ -0,0 +1,27 @@
+(function() {
+  $(document).ready(init);
+
+  function init() {
+    $('.show').on('click', show);
+    $('.hide').on('click', hide);
+
+    function show() {
+      var selector = $(this).attr('data-selector');
+      var options = getOptions();
+      Focusable.setFocus($(selector), options);
+    }
+
+    function hide() {
+      Focusable.hide();
+    }
+  }
+
+  function getOptions() {
+    return {
+      fadeDuration: parseInt($('#fade-duration').val()),
+      hideOnClick: $('#hide-on-click').is(':checked'),
+      hideOnESC: $('#hide-on-esc').is(':checked'),
+      findOnResize: $('#find-on-resize').is(':checked')
+    };
+  }
+})();
